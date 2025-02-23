967d05e559c8215bb0258d26d80c090b58049604
Maxence Simonart <M.G.G.Simonart@student.tudelft.nl>
2025-02-23 11:29:46 +0100

    Added torque, omega minimum, modified yearly energy and a semicolon in speed_profile

---
+++ Task 2 System Design/tipspeed_max.m
@@ -0,0 +1,6 @@
+function [tip_v_max] = tipspeed_max(U_r, lambda)
+% function that calculates the maximal tip speed in m/s, using as input the rated wind speed and the tip speed ratio
+
+    tip_v_max=lambda*U_r;
+
+end
--- Task 2 System Design/torque.m
+++ Task 2 System Design/torque.m
@@ -1,4 +1,4 @@
-function [Q] = torque (P_curve, U, D, lambda)
+function [Q] = torque(P_curve, U, D, lambda)
 % Function to obtain the torque Q in Nm, for any wind speed U in m/s, also using as inputs the power curve,
 % the rotor diameter D in m and the tip speed ratio lambda
 
