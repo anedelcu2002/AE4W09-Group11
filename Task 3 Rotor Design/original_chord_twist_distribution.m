1fcf9c554a2c8ab2696e4b5c805c3aa57d6dabdf
anedelcu2002 <127727911+anedelcu2002@users.noreply.github.com>
2025-03-05 14:06:27 +0100

    added code folders for task 3

---
+++ Task 3 Rotor Design/original_chord_twist_distribution.m
@@ -0,0 +1,3 @@
+%% Takes in original turbine (or scaled version) chord and twist distribution for comparison with designed turbine
+
+% to do: extract data from fasttool for NREL turbine, plot out, make comparison plot with data from chord_twist_calculator
