1161eb4174d8fb460492328f26a90e608a118d4c
anedelcu2002 <127727911+anedelcu2002@users.noreply.github.com>
2025-03-18 09:33:34 +0100

    Update structural_scaling.m

--- Task 3 Rotor Design/structural_scaling.m
+++ Task 3 Rotor Design/structural_scaling.m
@@ -23,12 +23,15 @@ MAIN
 % need to initialize this data for original turbine and for our turbine
 station_array=[];
 mass_array=[];
-thrust_array=CT*0.5*rho*U_rated^2*(pi*D^2/4);
+thrust_array=CT*0.5*rho*U_rated^2*(pi*D^2/4); % divide by three
 torque_array=CQ*0.5*rho*U_rated^3*(pi*D^2/4);
 twist_array=[];
+chord_array=[];
+airfoil_thickness_array=[];
 flap_stiffness_array=[]; % oriented by structural twist angle, assumed equal to aerodynamic twist angle
 edge_stiffness_array=[];
-chord_array=[];
+
+% airfoil distribution 0-20 cylinder, 20-40, 40-70, 70-100
 
 %% Original turbine response calculation
 
