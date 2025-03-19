14c982879635cd077b6c0e89b2f8ba23fae8783e
VictorDoomSlayer <victor.vass.2003@gmail.com>
2025-03-19 15:40:34 +0100

    Added Blended Airfoils and Cylinder

--- Task 3 Rotor Design/BEM.m
+++ Task 3 Rotor Design/BEM.m
@@ -3,23 +3,50 @@ clear
 close all
 
 %% Airfoil data
+Cylinder.dat = load("Cylinder.txt");
+Cylinder.alpha = Cylinder.dat(:,1);
+Cylinder.Cl = Cylinder.dat(:,2);
+Cylinder.Cd = Cylinder.dat(:,3);
 
 DU_airfoil.dat = load("DU_97W300.txt");
 DU_airfoil.alpha = DU_airfoil.dat(:,1);
 DU_airfoil.Cl = DU_airfoil.dat(:,2);
 DU_airfoil.Cd = DU_airfoil.dat(:,3);
 
+Blend1.dat = load("Blend DU_NACA.txt");
+Blend1.alpha = Blend1.dat(:,1);
+Blend1.Cl = Blend1.dat(:,2);
+Blend1.Cd = Blend1.dat(:,3);
+
 NACAMid.dat = load("NACA_644421.txt");
 NACAMid.alpha = NACAMid.dat(:,1);
 NACAMid.Cl = NACAMid.dat(:,2);
 NACAMid.Cd = NACAMid.dat(:,3);
+
+Blend2.dat = load("Blend NACA_NACA.txt");
+Blend2.alpha = Blend2.dat(:,1);
+Blend2.Cl = Blend2.dat(:,2);
+Blend2.Cd = Blend2.dat(:,3);
 
 NACAOut.dat = load("NACA_643218.txt");
 NACAOut.alpha = NACAOut.dat(:,1);
 NACAOut.Cl = NACAOut.dat(:,2);
 NACAOut.Cd = NACAOut.dat(:,3);
 
-AirfoilSet = [DU_airfoil NACAMid NACAOut];
+AirfoilSet = [Cylinder DU_airfoil Blend1 NACAMid Blend2 NACAOut];
+
+figure
+hold on
+plot(DU_airfoil.alpha,DU_airfoil.Cl,"DisplayName","DU 97-W-300")
+plot(Blend1.alpha,Blend1.Cl,"DisplayName","Blend DU NACA");
+plot(NACAMid.alpha,NACAMid.Cl,"DisplayName","NACA 644421");
+plot(Blend2.alpha,Blend2.Cl,"DisplayName","Blend NACA NACA");
+plot(NACAOut.alpha,NACAOut.Cl,"DisplayName","NACA 643218");
+xlim([-20 20])
+grid on
+legend("Location",'best')
+xlabel("\alpha");
+ylabel("C_l")
 
 %% Rotor Parameters
 
@@ -39,7 +66,7 @@ rho = 1.225;
 sigma_r = @(r) (B*Chord(r))/(2*pi*r); % Chord solidity
 
 %% BEM
-mu = linspace(0.2,1,100); %First entry depends on where cylinder ends
+mu = linspace(0,1,49); %First entry depends on where cylinder ends
 rRoot = mu(1)*R;
 dr = mu(2)-mu(1); % deltar
 mu = mu + 0.5*dr; % Shift radius location to be exactly in middle of segments
@@ -60,12 +87,18 @@ for j = 1:length(mu)
 
         end
         r =  R*mu(j); % What radial section are we looking at
-        if mu(j) < 0.4 % Inboard
+        if mu(j) < 0.2 % Cylinder
             Airfoil = AirfoilSet(1);
-        elseif mu(j) < 0.7 && mu(j) >= 0.4
+        elseif mu(j) < 0.35 && mu(j) >= 0.2  % DU 97-W-300
             Airfoil = AirfoilSet(2);
-        else
+        elseif mu(j) < 0.45 && mu(j) >= 0.35 % Blend 1
             Airfoil = AirfoilSet(3);
+        elseif mu(j) < 0.6 && mu(j) >= 0.45  % NACA 644
+            Airfoil = AirfoilSet(4);
+        elseif mu(j) < 0.8 && mu(j) >= 0.6   % Blend 2
+            Airfoil = AirfoilSet(5);
+        else                                 % NACA 643
+            Airfoil = AirfoilSet(6);
         end
         UR = U0*(1-a(i,j)); % Obtain axial velocity at rotor
         UTang = omega*r*(1+aprime(i,j)); % Obtain rotor-induced velocity
@@ -104,7 +137,19 @@ for j = 1:length(mu)
     cond = (aDiff <= 1e-5 && aPrimeDiff <= 1e-5);
     i = i + 1;
     end
+    FAxial(1,j) = Cx*0.5*rho*(Uapp^2)*Chord(r);
+    FTang(1,j) = Cy*0.5*rho*(Uapp^2)*Chord(r);
     CQ(1,j) = 4*aprime(i,j)*(1-a(i,j))*lambda*mu(j);
     CP(1,j) = 4*a(i,j)*(1-a(i,j))^2;
     CT(1,j) = 4*a(i,j)*(1-a(i,j));
 end
+
+figure
+hold on
+plot(mu,FAxial,"DisplayName","Axial Force")
+plot(mu,FTang,"DisplayName","Tangential Force")
+title("Force in each blade section along span")
+xlabel("Spanwise point \mu")
+ylabel("Force [N]")
+legend
+grid on
