90d6618da5e90556a34efac11659e30e48b398f9
Pjotrhee <pietervanrhee@outlook.com>
2025-03-21 22:07:52 +0100

    Add gbx mass estimation

--- Task 4 Drive Train/MAIN.m
+++ Task 4 Drive Train/MAIN.m
@@ -1,7 +1,9 @@
-%TASK 2 DAT
+%TASK 2 DATA
 P_r = 3.5*10^6; %W
 Omega_LSS_max = 1.0327; %rad/s
 Omega_LSS_min = 0.224; %rad/s
+Q_LSS_min = P_r/Omega_LSS_max;
+Q_LSS_max = P_r/Omega_LSS_min;
 
 %CONVERTER DATA
 f_min = 4; %Hz
@@ -9,16 +11,19 @@ f_max = 50; %Hz
 
 %GENERATOR DATA
 n = 4;
-F_d = 20; %kN/m^2
-r_s = 2; %m
+F_d = 20; %kN/m^2m ?????????????
+r_s = 2; %m        ?????????????
 
+%GEARBOX DATA
+stage_types = [true,false,false];
+
+%GEARBOX
 [Omega_HSS_min_allowed, Omega_HSS_max_allowed] = Omega_HSS_allowed_range(n, f_min, f_max); %rad/s
 f_HSS_min_allowed = Omega_HSS_min_allowed/(2*pi);
 f_HSS_max_allowed = Omega_HSS_max_allowed/(2*pi);
 
-r_gbx = gbx_ratio_total(Omega_LSS_max, Omega_LSS_min, Omega_HSS_max_allowed, Omega_HSS_min_allowed, 150)
-stage_types = [true,false,false];
-r_list = gbx_design(r_gbx, 3, stage_types);
+r_goal = gbx_ratio_total(Omega_LSS_max, Omega_LSS_min, Omega_HSS_max_allowed, Omega_HSS_min_allowed, 150)
+r_list = gbx_design(r_goal, 3, stage_types);
 r_total = prod(r_list)
 
 [Omega_HSS_min, Omega_HSS_max] = Omega_HSS_used_range(r_total, Omega_LSS_min, Omega_LSS_max);
@@ -26,7 +31,14 @@ f_HSS_min = Omega_HSS_min/(2*pi);
 f_HSS_max = Omega_HSS_max/(2*pi);
 
 eff_gbx = gbx_eff(r_list,stage_types)
+M_gbx = gbx_mass(Q_LSS_min) %Q_RATED
+P_gbx = eff_gbx*P_r;
 
+Q_HSS_min = P_gbx/Omega_HSS_max;
+Q_HSS_max = P_gbx/Omega_HSS_min;
+
+
+%GENERATOR 
 V_gen = gen_sizing(F_d, P_r, Omega_HSS_max, eff_gbx);
 l_s = gen_length(V_gen, r_s);
 
--- Task 4 Drive Train/gbx_eff.m
+++ Task 4 Drive Train/gbx_eff.m
@@ -1,17 +1,13 @@
 function [eff_gbx] = gbx_eff(r_list, g_types)
-    eff_list1 = [];
-    eff_list2 = [];
+    eff_list = [];
     for i = 1:length(r_list)
         if g_types(i) == true
-            eff0 = 0.97;
+            eff0 = 0.99;
         elseif g_types(i) == false
             eff0 = 0.98;
         end
-
-        eff_list1(i) = eff0 * exp(-0.001 * (r_list(i)-1));
-        eff_list2(i) = eff0;
+        eff_list(i) = eff0;
     end
 
-    %eff_gbx = prod(eff_list1);
-    eff_gbx = prod(eff_list2);
+    eff_gbx = prod(eff_list);
 end
