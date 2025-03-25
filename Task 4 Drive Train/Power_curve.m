9fe6ed7b8d39da70fcd1e53bf3bc99ac2f84a585
Pjotrhee <pietervanrhee@outlook.com>
2025-03-25 10:20:06 +0100

    Added cable losses in P curve

--- Task 4 Drive Train/Power_curve.m
+++ Task 4 Drive Train/Power_curve.m
@@ -3,6 +3,8 @@ function [P_curve,U_r,U_range] = Power_curve(P_r_LSS,rho,U_ci,U_co,C_p,D,eff)
     % U_ci and U_co in m/s from part 2, the power coefficient C_p from part 3, the diameter D in m from part 2, the efficiency eff_total
 
     R = 0.5*D;
+    resistance = 0.009975;
+    votlage = 600;
 
     U_r = (P_r_LSS/(0.5*rho*C_p*pi*R^2))^(1/3);
 
@@ -14,8 +16,10 @@ function [P_curve,U_r,U_range] = Power_curve(P_r_LSS,rho,U_ci,U_co,C_p,D,eff)
             P_curve(i) = 0;
         elseif U_range(i) <= U_r
             P_curve(i) = 0.5*eff*rho*C_p*pi*(R^2)*(U_range(i)^3);
+            P_curve(i) = P_curve(i) - 3*(P_curve(i)/(3*600))^2*resistance;
         elseif U_range(i) < U_co
             P_curve(i) = P_r_LSS*eff;
+            P_curve(i) = P_curve(i) - 3*(P_curve(i)/(3*600))^2*resistance;
         else
             P_curve(i) = 0;
         end
